51551b6d181620e09be9a29535d5b289ac23c265
Gupta <arpit.ak.gupta@accenture.com>
2025-03-04 12:39:02 +0530

    changes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function message(){
     let name = document.getElementById('name').value;
 let email = document.getElementById('email').value;
 let text = document.getElementById('text').value;
-    alert(name+ ' ' + email + ' ' + text);
+   console.log(name+ ' ' + email + ' ' + text);
 }
 
 
