a9d5ca9cc12bd49fb45eb78579736ac936d67f38
Gupta <arpit.ak.gupta@accenture.com>
2025-03-04 10:58:41 +0530

    Initial commit - Bootstrap landing page

---
+++ script.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+// Scroll to Top Button
+let scrollTopBtn = document.getElementById("scrollTopBtn");
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 300) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+};
+
+scrollTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
